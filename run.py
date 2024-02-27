f1f13d936f994d443daffc94b22545af1b4fdfa1
Charles <chcarroll@wisc.edu>
2024-02-27 14:51:54 -0600

    save run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import operations
 #from operations import init
 #from operations import submit_calculations
+import project
 
+#operations.transfer_shit()
+#project.transfer("074898814e96499f05f999c03fde372d")
 operations.init()
-#operations.submit_calculations()
+operations.submit_calculations()
